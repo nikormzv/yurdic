09ed1d82b1f589eeb547ab0f1a96da3241ec9174
nikormzv <nikormzv@ukr.net>
2026-07-26 19:00:58 +0300

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,33 @@
+DATA = [
+
+    {
+
+        "service": "GitHub",
+
+        "username": "developer",
+
+        "password": "Code123!"
+
+    },
+
+    {
+
+        "service": "Google",
+
+        "username": "user@gmail.com",
+
+        "password": "Summer2025!"
+
+    },
+
+    {
+
+        "service": "Steam",
+
+        "username": "gamer",
+
+        "password": "Game987#"
+
+    }
+
+]
